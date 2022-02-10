61bfb8c8817ed0acf455374bc16142ea926ee874
Luís Gomes <luis.gomes@liferay.com>
2022-02-10 16:33:32 -0300

    Persists data in local storage and fix api requests

--- src/contexts/repositories.tsx
+++ src/contexts/repositories.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import * as Api from "../api/repository";
+import { checkTypeOfUser } from "../api/Type";
 
 const RepositoryContext = createContext({} as RepositoryContext);
 
@@ -11,18 +12,47 @@ export const RepositoryProvider: React.FC = ({ children }) => {
   const addRepository = async () => {
     const response = await Api.getRepository(urlEnding);
     setRepositories([...repositories, response]);
+    localStorage.setItem("repositories", JSON.stringify([response]));
     setUrlEnding("");
   };
 
   const addAllUserRepositories = async () => {
-    const response = await Api.getAllUserRepositories(urlEnding);
-    setRepositories(repositories.concat(response));
+    const type = await checkTypeOfUser(urlEnding);
+    if (type === "User") {
+      const response = await Api.getAllUserRepositories(urlEnding);
+      setRepositories(repositories.concat(response));
+      localStorage.setItem(
+        "repositories",
+        JSON.stringify(repositories.concat(response))
+      );
+    }
+    if (type === "Organization") {
+      const response = await Api.getAllOrganizationRepositories(urlEnding);
+      setRepositories(repositories.concat(response));
+      localStorage.setItem(
+        "repositories",
+        JSON.stringify(repositories.concat(response))
+      );
+    }
     setUrlEnding("");
   };
 
   const deleteRepository = (id: number) => {
-    setRepositories(repositories.filter((e) => e.id !== id));
+    const filteredRepositoryArray = repositories.filter((e) => e.id !== id);
+    setRepositories(filteredRepositoryArray);
+    localStorage.setItem(
+      "repositories",
+      JSON.stringify(filteredRepositoryArray)
+    );
   };
+
+  const getRepositories = () => {
+    setRepositories(JSON.parse(localStorage.getItem("repositories") || "[]"));
+  };
+
+  useEffect(() => {
+    getRepositories();
+  }, []);
 
   return (
     <RepositoryContext.Provider
